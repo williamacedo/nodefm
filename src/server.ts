136310c2154f8fc1a4d22d9b34a6f58c249dd295
William Macedo <williamcedo37@yahoo.com.br>
2023-11-27 07:38:14 -0300

    implemeting middleware

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,23 @@
 import express from 'express'
+import router from './router'
+import morgan from 'morgan'
 
 const app = express()
 
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use((req, res, next) => {
+    req.shhhh_secret = 'doggy'
+    next()
+})
 app.get('/', (req, res) => {
     console.log('hello from express');
     res.status(200)
     res.json({ message: 'hello' })
 })
+
+app.use('/api', router)
 
 export default app
