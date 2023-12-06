ba1be53dc0c0e4b7688cc4083ad7a4efe663040f
William Macedo <williamcedo37@yahoo.com.br>
2023-12-06 08:17:49 -0300

    add logic to update

--- src/router.ts
+++ src/router.ts
@@ -1,33 +1,34 @@
 import { Router } from 'express'
 import {  body, oneOf } from 'express-validator'
 import { handleInputErrors } from './modules/middleware'
+import { createProduct, deleteProduct, getOneProduct, getProducts, updateProduct } from './handlers/product'
+import { createUpdate, deleteUpdate, getOneUpdate, getUpdates, updateUpdate } from './handlers/update'
 
 const router = Router()
 
-router.get('/product', (req, res) => {
-    res.json({ message: 'hello' })
-})
-router.get('/product/:id', () => {})
-router.put('/product/:id', body('name').isString(), handleInputErrors, (req, res) => {})
-router.post('/product', body('name').isString(), handleInputErrors, () => {})
-router.delete('/product/:id', () => {})
+router.get('/product', getProducts)
+router.get('/product/:id', getOneProduct)
+router.put('/product/:id', body('name').isString(), handleInputErrors, updateProduct)
+router.post('/product', body('name').isString(), handleInputErrors, createProduct)
+router.delete('/product/:id', deleteProduct)
 
 
-router.get('/update', () => {})
-router.get('/update/:id', () => {})
+router.get('/update', getUpdates)
+router.get('/update/:id', getOneUpdate)
 router.put('/update/:id', 
     body('title').optional(), 
     body('body').optional(), 
-    body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']),
+    body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']).optional(),
     body('version').optional(),  
-    () => {}
+    updateUpdate
 )
 router.post('/update', 
 body('title').exists(), 
 body('body').exists().isString(), 
-() => {}
+body('productId').exists().isString(),
+createUpdate
 )
-router.delete('/update/:id', () => {})
+router.delete('/update/:id', deleteUpdate)
 
 router.get('/updatepoint', () => {})
 router.get('/updatepoint/:id', () => {})
