b6fcbb6bfa255c1faa81c09bd56ec9b161a61758
William Macedo <williamcedo37@yahoo.com.br>
2023-12-11 08:42:51 -0300

    add error handle logic

--- src/server.ts
+++ src/server.ts
@@ -26,4 +26,15 @@ app.use('/api', protect, router)
 
 app.post('/user', createNewUser)
 app.post('/signin', signin)
+
+app.use((err, req, res, next) => {
+    if (err.type === 'auth') {
+        res.status(401).json({ message: 'unauthorized' })
+    } else if (err.type === 'input') {
+        res.status(400).json({ message: 'invalid input' })
+    } else {
+        res.status(500).json({ message: 'oops, thats on us' })
+    }
+})
+
 export default app
